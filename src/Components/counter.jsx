0efb11dcd974e80af2ae50907d569bcd55b1d01b
Nabeel73 <32979012+Nabeel73@users.noreply.github.com>
2019-01-16 23:16:33 +0500

    Remove previous code then complied
    Removing previous code then complied simply display tags

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -2,66 +2,20 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state  = {
-        count : 1,
-        //imageUrl:"https://picsum.photos/200/300?image=0"
+        count : 0,
+       // imageUrl:"https://picsum.photos/200/300?image=0",
         tags :['tag1','tag2','tag3']
-
     };
-    
-    renderTags()         {
-           
-        if (this.state.tags === 0 ) return <p>There is no Tags!</p>;
-        return <ul>{this.state.tags.map(tags=><li key={tags}>{tags}</li>)}</ul>
+    rendersTags(){
+
+        if(this.state.tags.lenght === 0) return <p>There is no tags</p>;
+        return <ul> {this.state.tags.map(tag=> <li key={tag}>[tag}</li>)}</ul>;
     }
-    styles = {
-        fontSize : 24,
-        fontWeight : "bold",
-        fontFamily: 'Times New Roman',
-        
-    };
-        render() { 
-            let classes = "badge m-2 badge-";
-            classes += (this.state.count === 0) ? "warning" : "primary";
+    render(){
+        return(
+            <div>
+          {this.rendersTags()}
+            </div> );
+    }}
 
-          
-      return( 
-        <div>
-        <img src={this.state.imageUrl}  alt=""/>
-        <span>{this.formatcount()}</span><br></br>
-        <span>{5*5}</span>
-   
-        <hr></hr>
-        <span style={this.styles} className="badge badge-warning m-3">{this.formatcount()}</span>
-        <span className={classes}>{this.formatcount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-       {/* <ul>{this.state.tags.map(tags=><li key={tags}>{tags}</li>)}</ul>*/}
-        {this.renderTags() }
-        </div>
-        );
-    }
-    myMethodTest() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
-
-   // getBadgeClasses() {
-     // let classes = "badge m-2 badge-";
-      // classes += (this.state.count === 0) ? "warning" : "primary";
-      // return classes;
-    //};
-
-     formatcount(){
-     //    const { count } = this.state;
-       // return this.state.count === 0 ?  'Zero' : this.state.count;
-      // return count === 0 ?  'Zero' : this.state.count;
-
-     }
-}
-    //formatCount(){
-    //const { count } = this.state;
-   // return this.state.count === 0 ? : this.state.count;
-   // return count === 0 ? "Zero":count;
-
- 
 export default Counter;
