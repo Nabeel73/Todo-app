2ce549c172153f0c446b235ef8fd26e5431f17e7
Nabeel73 <32979012+Nabeel73@users.noreply.github.com>
2019-01-20 14:25:27 +0500

    array tag error

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -3,19 +3,20 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state  = {
         count : 0,
-       // imageUrl:"https://picsum.photos/200/300?image=0",
-        tags :['tag1','tag2','tag3']
+       //imageUrl:"https://picsum.photos/200/300?image=0",
+        tags : []
     };
     rendersTags(){
-
-        if(this.state.tags.lenght === 0) return <p>There is no tags</p>;
-        return <ul> {this.state.tags.map(tag=> <li key={tag}>[tag}</li>)}</ul>;
+        if(this.state.tags.length === 0) return <p>There is no tags!</p>;
+        return <ul> {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
     }
     render(){
         return(
-            <div>
+            <div>    
+          {this.state.tags.length === 0 && "Please create new tags!"}
           {this.rendersTags()}
             </div> );
-    }}
+    }
+}
 
 export default Counter;
