026cad3fed56a2684c0e154533d2c885035a2b33
Nabeel73 <32979012+Nabeel73@users.noreply.github.com>
2019-01-27 21:07:25 +0500

    code compiled
    code compiled im cmd but error in browser

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -2,20 +2,18 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state  = {
-        count : 0,
+        count : 0
        //imageUrl:"https://picsum.photos/200/300?image=0",
       //  tags : []
     };
-
     handlerIncrement(){
         console.log('Increment Clicked');
-
     }
     render(){
         return(
             <div>
-            <span className={this.getBadgeClasses()}>{this.getformatCount()}</span>
-            <button onClick={this.handlerIncrement} className="btn btn secondary btn-sm">Increment</button>
+             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button onClick={this.handlerIncrement} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
@@ -23,10 +21,6 @@ class Counter extends Component {
        let classes = "badge m-2 badge-";
         classes += this.state.count === 0 ? "warning" : "primary";
         return classes;
-       };
-
-
+    };
 }
-
-
 export default Counter;
