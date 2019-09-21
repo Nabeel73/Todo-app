7a2f3cbf5d51c6eda749da2a8734d353c903cae4
Nabeel73 <32979012+Nabeel73@users.noreply.github.com>
2019-09-21 23:02:18 +0500

    add button

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
-
 class Counter extends Component {
     state  = {
-        count : 0
+        count : 1
        //imageUrl:"https://picsum.photos/200/300?image=0",
       //  tags : []
     };
+    constructor(){
+     // console.log("Constructor",this );
+     super()
+    this.handlerIncrement = this.handlerIncrement.bind(this);
+    } 
+
     handlerIncrement(){
         console.log('Increment Clicked');
     }
     render(){
         return(
             <div>
-         {/*    <span className={this.getBadgeClasses()}>{this.formatCount()}</span>*/}
+           {/*<span className={this.getBadgeClasses()}>{this.formatCount()}</span>*/}
             <button onClick={this.handlerIncrement} className="btn btn-secondary btn-sm">Increment</button>
+            
+            <button onClick={this.handlerIncrement} className="btn btn-secondary btn-sm">Increment</button>
+            
+
+            
             </div>
+            
         );
     }
      getBadgeClasses() {
