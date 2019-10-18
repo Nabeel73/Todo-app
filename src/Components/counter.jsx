2884cf16314fe7f8342fc6eb54d156ae303e495b
Nabeel73 <32979012+Nabeel73@users.noreply.github.com>
2019-10-18 22:41:35 +0500

    add button class

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -9,17 +9,26 @@ class Counter extends Component {
      // console.log("Constructor",this );
      super()
     this.handlerIncrement = this.handlerIncrement.bind(this);
+    this.addNumber.setstate = this.addNumber.bind(this);
     } 
 
     handlerIncrement(){
         console.log('Increment Clicked');
+    }
+    addNumber(){
+        document.write('');
     }
     render(){
         return(
             <div>
                 <h1>Hello Counter App</h1>
                 <span>{this.state.count}</span><br/><br/><br/>
+                <button onClick={this.addNumber} className="btn btn-secondary btn-sm">Add</button> 
+                <button onClick={this.handlerIncrement} className="btn btn-danger btn-sm">Delete</button><br/><br/><br/>
                 <button>Add</button>          <button>Delete</button><br/><br/><br/>
+                <button>Add</button>          <button>Delete</button><br/><br/><br/>
+                <button>Add</button>          <button>Delete</button><br/><br/><br/>
+            
             <button onClick={this.handlerIncrement} className="btn btn-secondary btn-sm">Increment</button>
            
            {/*<span className={this.getBadgeClasses()}>{this.formatCount()}</span>*/}
