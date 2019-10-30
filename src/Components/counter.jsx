d5bc82b0977e802fe4735a8441305fad07d3edef
Nabeel73 <32979012+Nabeel73@users.noreply.github.com>
2019-10-30 23:02:51 +0500

    code error remove

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,43 +1,32 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state  = {
-      count : 1,
-      imageUrl:"https://picsum.photos/200/300?image=0",
-     tags : []
-    };
-    constructor(){
-     // console.log("Constructor",this );
-     super()
-    this.handlerIncrement = this.handlerIncrement.bind(this);
-    this.addNumber.setstate = this.addNumber.bind(this);
-    } 
-
-    handlerIncrement(){
-        console.log('Increment Clicked');
-    }
-    addNumber(){
-        document.write('');
-    }
+      count : 0
+      };
+     // constructor(){
+       //   super();
+         // this.handlerIncrement.bind(this);
+    //  }
+    handlerIncrement = () => {
+       // this.state.count++;
+        this.setState({ count: this.state.count + 1});
+     //console.log("Increment clicked",this);
+     }
     render(){
         return(
             <div>
-                <h1>Hello Counter App</h1>
-                <img src={this.state.imageUrl} alt=""></img>
-                <span>{this.state.count}</span><br/><br/><br/>
-                <button onClick={this.addNumber} className="btn btn-secondary btn-sm">Add</button> 
-                <button onClick={this.handlerIncrement} className="btn btn-danger btn-sm">Delete</button><br/><br/><br/>
-                <button>Add</button>          <button>Delete</button><br/><br/><br/>
-                <button>Add</button>          <button>Delete</button><br/><br/><br/>
-                <button>Add</button>          <button>Delete</button><br/><br/><br/>
-            
-            <button onClick={this.handlerIncrement} className="btn btn-secondary btn-sm">Increment</button>
-           
-           {/*<span className={this.getBadgeClasses()}>{this.formatCount()}</span>*/}
-                              
-            </div>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.handlerIncrement}
+                   className="btn btn-secondary btn-sm">
+                       Increment
+                       </button>
+          </div>
+       );
+    }
+    formatCount(){
+        const { count } = this.state;
+        return count === 0 ? "Zero"  : count;     
 
-            
-        );
     }
      getBadgeClasses() {
        let classes = "badge m-2 badge-";
